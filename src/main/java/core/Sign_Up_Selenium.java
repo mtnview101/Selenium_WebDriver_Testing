5fe7b50482778994e2bdc6f61c85beb97ef20639
mtnview101 <mtnview101@gmail.com>
2017-01-15 23:18:22 -0800

    third commit

--- src/main/java/core/Sign_Up_Selenium.java
+++ src/main/java/core/Sign_Up_Selenium.java
@@ -1,17 +1,137 @@
 package core;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
 public class Sign_Up_Selenium {
-	public static void main(String[] args) {
+	
+    static WebDriver driver;
 
-		WebDriver driver = new HtmlUnitDriver();
-		//WebDriver driver = new FirefoxDriver();
+    public static void setWebDriver(String browser) throws IOException {
+           Logger logger = Logger.getLogger("");
+           logger.setLevel(Level.OFF);
+
+           String driverPath = "";
+  if ((browser == "Firefox") && (System.getProperty("os.name").toUpperCase().contains("MAC"))) 
+           {driverPath = "./src/main/resources/webdrivers/mac/geckodriver.sh";}
+
+else if ((browser == "Firefox") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))) 
+           {driverPath = "./src/main/resources/webdrivers/pc/geckodriver.exe";}
+
+else if ((browser == "Chrome") && (System.getProperty("os.name").toUpperCase().contains("MAC"))) 
+           {driverPath = "./src/main/resources/webdrivers/mac/chromedriver";}
+
+else if ((browser == "Chrome") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))) 
+           {driverPath = "./src/main/resources/webdrivers/pc/chromedriver.exe";}
+
+else if ((browser == "Safari") && (System.getProperty("os.name").toUpperCase().contains("MAC")))
+          {}
+
+else if ((browser == "Safari") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))) 
+          {throw new IllegalArgumentException("Safari is not available for Windows");}
+
+else if ((browser == "IE") && (System.getProperty("os.name").toUpperCase().contains("MAC"))) 
+          {throw new IllegalArgumentException("Internet Explorer is not available for macOS");}
+
+else if ((browser == "IE") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))) 
+          {driverPath = "./src/main/resources/webdrivers/pc/IEDriverServer.exe";}
+
+else if ((browser == "Edge") && (System.getProperty("os.name").toUpperCase().contains("MAC"))) 
+          {throw new IllegalArgumentException("Microsoft Edge is not available for macOS");}
+
+else if ((browser == "Edge") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS")))
+          {driverPath = "./src/main/resources/webdrivers/pc/MicrosoftWebDriver.exe";}
+
+           else if (browser == "HtmlUnit") {}
+
+           else   {throw new IllegalArgumentException("Unknown OS");}
+
+           switch (browser) {
+
+           case "Firefox":
+                  System.setProperty("webdriver.gecko.driver", driverPath);
+                  driver = new FirefoxDriver();
+                  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                  driver.manage().window().maximize();
+                  break;
+
+           case "Chrome":
+                  System.setProperty("webdriver.chrome.driver", driverPath);
+                  System.setProperty("webdriver.chrome.silentOutput", "true");
+                  ChromeOptions option = new ChromeOptions();
+                  if (System.getProperty("os.name").toUpperCase().contains("MAC")) 
+                  {option.addArguments("-start-fullscreen");}
+                  else if (System.getProperty("os.name").toUpperCase().contains("WINDOWS")) 
+                  {option.addArguments("--start-maximized");}
+                  else {throw new IllegalArgumentException("Unknown OS");}
+                  driver = new ChromeDriver(option);
+                  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                  break;
+
+           case "Safari":
+                  SafariOptions options = new SafariOptions();
+                  options.setUseCleanSession(true);
+                  options.setPort(55555);
+                  driver = new SafariDriver(options);
+                  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                  driver.manage().window().maximize();
+                  break;
+
+           case "IE":
+                 DesiredCapabilities IEDesiredCapabilities = DesiredCapabilities.internetExplorer();
+					 IEDesiredCapabilities.setCapability (InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+					 IEDesiredCapabilities.setCapability (InternetExplorerDriver.INITIAL_BROWSER_URL, "");
+                  IEDesiredCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+                  IEDesiredCapabilities.setJavascriptEnabled(true);
+                  IEDesiredCapabilities.setCapability("enablePersistentHover", false);
+
+                  System.setProperty("webdriver.ie.driver", driverPath);
+                  driver = new InternetExplorerDriver(IEDesiredCapabilities);
+                  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                  driver.manage().window().maximize();
+                  break;
+
+           case "Edge":
+                  System.setProperty("webdriver.edge.driver", driverPath);
+                  driver = new EdgeDriver();
+                  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                  driver.manage().window().maximize();
+                  break;
+
+           case "HtmlUnit":
+                  driver = new HtmlUnitDriver();
+                  ((HtmlUnitDriver) driver).setJavascriptEnabled(true);
+                  break;
+
+           default:
+                  throw new IllegalArgumentException("Unknown Broweser");
+           }
+    }
+	
+	public static void main(String[] args) throws IOException {
+		
+	   	 String in_browser = "Firefox"; // "HtmlUnit" "Firefox" or "Chrome" or Safari or IE or Edge
+         setWebDriver(in_browser);
+
+
+		//WebDriver driver = new HtmlUnitDriver();
+		WebDriver driver = new FirefoxDriver();
 		String text_case_id_01 = "TC-001.01";
 		String text_case_id_02 = "TC-001.02";
 		String text_case_id_03 = "TC-001.03";
@@ -43,7 +163,7 @@ public class Sign_Up_Selenium {
 
 		// TC-001.02
 
-/*		driver.findElement(By.id("id_img_facebook")).click();
+		driver.findElement(By.id("id_img_facebook")).click();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		ArrayList<String> allTabs = new ArrayList<String>(
 		driver.getWindowHandles());
@@ -59,7 +179,7 @@ public class Sign_Up_Selenium {
 		
 	  else {System.out.println("Test Case ID: \t\t" + text_case_id_02 + " - FAILED");
 			System.out.println("Title Expected/Actual: \t" + title_facebook_expected + "/" + title_facebook_actual);
-			System.out.println("=======================================");}*/
+			System.out.println("=======================================");}
 
 		// TC-001.03
 
